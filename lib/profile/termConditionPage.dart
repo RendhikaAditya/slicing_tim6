1d1ac5bb8f9ba23456ca959b3db1ad169ef9781b
Yuli <antiyuli2828@gmail.com>
2024-03-25 20:54:44 +0700

    finish security and term & conditions

--- lib/profile/termConditionPage.dart
+++ lib/profile/termConditionPage.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-class TermConditionPage extends StatefulWidget {
-
-  @override
-  State<TermConditionPage> createState() => _TermConditionPageState();
-}
-
-class _TermConditionPageState extends State<TermConditionPage> {
+class TermConditionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xFFF5F9FF), // Ubah warna app bar
         title: Text(
-          'TermCondition',
+          'Terms & Conditions',
           style: TextStyle(
             fontFamily: 'Jost',
             fontSize: 21,
@@ -22,7 +16,61 @@ class _TermConditionPageState extends State<TermConditionPage> {
           ),
         ),
       ),
-      body: Center(child: Text("s"),),
+      backgroundColor: Color(0xFFF5F9FF), // Ubah warna latar belakang body
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: 'Condition & Attending\n',
+                  style: TextStyle(
+                    fontFamily: 'Jost',
+                    fontSize: 18,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                TextSpan(
+                  text:
+                      'At enim hic etiam dolore. Dulce amarum, leve asperum, prope longe, stare movere, quadratum rotundum. At certe gravius. Nullus est igitur cuiusquam dies natalis. Paulum, cum regem Persem captum adduceret, eodem flumine invectio?\n\nQuare hoc videndum est, possitne nobis hoc ratio philosophorum dare. Sed finge non solum callidum eum, qui aliquid improbe faciat, verum etiam praepotentem, ut M. Est autem officium, quod ita factum est, ut eius facti probabilis ratio reddi possit\n',
+                  style: TextStyle(
+                    fontFamily: 'Mulish',
+                    fontSize: 15,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: 'Terms & Use\n',
+                  style: TextStyle(
+                    fontFamily: 'Jost',
+                    fontSize: 18,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                TextSpan(
+                  text:
+                      'Ut proverbia non nulla veriora sint quam vestra dogmata. Tamen aberramus a proposito, et, ne longius, prorsus, inquam, Piso, si ista mala sunt, placet. Omnes enim iucundum motum, quo sensus hilaretur. Cum id fugiunt, re eadem defendunt, quae Peripatetici, verba. Quibusnam praeteritis? Portenta haec esse dicit, quidem hactenus; Si id dicis, vicimus. Qui ita affectus, beatum esse numquam probabis; Igitur neque stultorum quisquam beatus neque sapientium non beatus.\n\nDicam, inquam, et quidem discendi causa magis, quam quo te aut Epicurum reprehensum velim. Dolor ergo, id est summum malum, metuetur semper, etiamsi non ader.\n',
+                  style: TextStyle(
+                    fontFamily: 'Mulish',
+                    fontSize: 15,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
