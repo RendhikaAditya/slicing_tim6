9521aa3fdcb9ba70a20e6a8bb558178b1e8d3889
Yuli <antiyuli2828@gmail.com>
2024-03-26 19:10:09 +0700

    add message

---
+++ lib/model/model_messages.dart
@@ -0,0 +1,17 @@
+// class Messages {
+//   final String chat;
+//   final String time;
+
+//   Messages({
+//     required this.chat,
+//     required this.time,
+//   });
+// }
+
+// // Contoh data
+// List<Messages> messages = [
+//   Messages(
+//     chat: 'Hi, Nicholas Good Evening',
+//     time: '10:45',
+//   ),
+// ];
